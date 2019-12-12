551fc5ed469c2f6c255207c425bc0e2424f57ba1
mob-sakai <sakai861104@gmail.com>
2019-12-13 00:46:09 +0900

    feat: update compiler to 1.1.1

--- Packages/com.coffee.internal-accessible-compiler/Compiler~/src/Options.cs
+++ Packages/com.coffee.internal-accessible-compiler/Compiler~/src/Options.cs
@@ -5,54 +5,54 @@ using System.Collections.Generic;
 
 namespace InternalAccessibleCompiler
 {
-	/// <summary>
-	/// Command line options.
-	/// </summary>
-	public class Options
-	{
-		/// <summary>
-		/// Output path.
-		/// </summary>
-		[Option('o', "output", Required = true, Default = "", HelpText = "Output path")]
-		public string Output { get; set; }
+    /// <summary>
+    /// Command line options.
+    /// </summary>
+    public class Options
+    {
+        /// <summary>
+        /// Output path. If it is empty, a dll is generated in the same path as csproj.
+        /// </summary>
+        [Option('o', "output", Required = false, Default = "", HelpText = "Output path. If it is empty, a dll is generated in the same path as csproj.")]
+        public string Output { get; set; }
 
 
-		/// <summary>
-		/// Assembly names to access internal.
-		/// </summary>
-		[Option('a', "assemblyNames", Required = true, Default = "", Separator = ';', HelpText = "Assembly names to access internal")]
-		public IEnumerable<string> AssemblyNames { get; set; }
+        /// <summary>
+        /// Target assembly names separated by semicolons to access internally.
+        /// </summary>
+        [Option('a', "assemblyNames", Required = false, Separator = ';', HelpText = "Target assembly names separated by semicolons to access internally")]
+        public IEnumerable<string> AssemblyNames { get; set; }
 
-		/// <summary>
-		/// Configuration.
-		/// </summary>
-		[Option('c', "configuration", Required = false, Default = OptimizationLevel.Release, HelpText = "Configuration")]
-		public OptimizationLevel Configuration { get; set; }
+        /// <summary>
+        /// Configuration.
+        /// </summary>
+        [Option('c', "configuration", Required = false, Default = OptimizationLevel.Release, HelpText = "Configuration")]
+        public OptimizationLevel Configuration { get; set; }
 
-		/// <summary>
-		/// Logfile path.
-		/// </summary>
-		[Option('l', "logfile", Required = false, Default = "compile.log", HelpText = "Logfile path")]
-		public string Logfile { get; set; }
+        /// <summary>
+        /// Logfile path.
+        /// </summary>
+        [Option('l', "logfile", Required = false, Default = "compile.log", HelpText = "Logfile path")]
+        public string Logfile { get; set; }
 
-		/// <summary>
-		/// Input .csproj path.
-		/// </summary>
-		[Value(1, MetaName = "ProjectPath", HelpText = "Input .csproj path")]
-		public string ProjectPath { get; set; }
+        /// <summary>
+        /// Input .csproj path.
+        /// </summary>
+        [Value(1, MetaName = "ProjectPath", HelpText = "Input .csproj path")]
+        public string ProjectPath { get; set; }
 
-		/// <summary>
-		/// Usages.
-		/// </summary>
-		[Usage(ApplicationAlias = "InternalAccessibleCompiler")]
-		public static IEnumerable<Example> Examples
-		{
-			get
-			{
-				return new List<Example>() {
-					new Example("Compile your project to internal accessible dll", new Options { Output = "your.dll", ProjectPath = "your.csproj" })
-				};
-			}
-		}
-	}
+        /// <summary>
+        /// Usages.
+        /// </summary>
+        [Usage(ApplicationAlias = "InternalAccessibleCompiler")]
+        public static IEnumerable<Example> Examples
+        {
+            get
+            {
+                return new List<Example>() {
+                    new Example("Compile your project to internal accessible dll", new Options { Output = "your.dll", ProjectPath = "your.csproj" })
+                };
+            }
+        }
+    }
 }
--- Packages/com.coffee.internal-accessible-compiler/Compiler~/src/Program.cs
+++ Packages/com.coffee.internal-accessible-compiler/Compiler~/src/Program.cs
@@ -2,18 +2,18 @@
 
 namespace InternalAccessibleCompiler
 {
-	public class Program
-	{
-		/// <summary>
-		/// Entry point.
-		/// </summary>
-		/// <param name="args"></param>
-		public static int Main(string[] args)
-		{
-			int exitCode = 0;
-			Parser.Default.ParseArguments<Options>(args)
-				.WithParsed(opt => exitCode = Compiler.Compile(opt));
-			return exitCode;
-		}
-	}
+    public class Program
+    {
+        /// <summary>
+        /// Entry point.
+        /// </summary>
+        /// <param name="args"></param>
+        public static int Main(string[] args)
+        {
+            int exitCode = 0;
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(opt => exitCode = Compiler.Compile(opt));
+            return exitCode;
+        }
+    }
 }
