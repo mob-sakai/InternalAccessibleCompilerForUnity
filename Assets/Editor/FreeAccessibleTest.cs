3593ba84d560de514c9920fb5a83f4a11e940e1f
mob-sakai <sakai@mobcast.jp>
2019-12-16 13:26:16 +0900

    Test: Add simple test

---
+++ Assets/Editor/FreeAccessibleTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Coffee.FreeAccessible
+{
+	public class FreeAccessibleTest
+	{
+		[UnityEditor.MenuItem("FreeAccessibleTest/CallDelayed 10s")]
+		static void RegisterCallDelayed()
+		{
+			UnityEditor.EditorApplication.CallDelayed(CallDelayed, 10);
+		}
+
+		static void CallDelayed()
+		{
+			Debug.Log("10 seconds!");
+		}
+	}
+}
