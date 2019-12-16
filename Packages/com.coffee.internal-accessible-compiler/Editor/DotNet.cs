5623c35bb320b5d9a721994e859ae22429205a85
mob-sakai <sakai@mobcast.jp>
2019-12-16 15:11:21 +0900

    fix: catch exception

--- Packages/com.coffee.internal-accessible-compiler/Editor/DotNet.cs
+++ Packages/com.coffee.internal-accessible-compiler/Editor/DotNet.cs
@@ -77,16 +77,24 @@ namespace Coffee.InternalAccessibleCompiler
             };
             UnityEngine.Debug.LogFormat("[DotNet.Execute] {0} {1}", startInfo.FileName, args);
 
-            var p = Process.Start(startInfo);
-            if (p == null || p.Id == 0 || p.HasExited)
+            try
             {
-                UnityEngine.Debug.LogError("[DotNet.Execute] dotnet not found. To install additional .NET Core runtimes or SDKs: https://aka.ms/dotnet-download");
-                resultCallback(false, "", "");
-                return;
-            }
+                var p = Process.Start(startInfo);
+                if (p == null || p.Id == 0 || p.HasExited)
+                {
+                    UnityEngine.Debug.LogError("[DotNet.Execute] dotnet not found. To install additional .NET Core runtimes or SDKs: https://aka.ms/dotnet-download");
+                    resultCallback(false, "", "");
+                    return;
+                }
 
-            p.WaitForExit();
-            resultCallback(p.ExitCode == 0, p.StandardOutput.ReadToEnd(), p.StandardError.ReadToEnd());
+                p.WaitForExit();
+                resultCallback(p.ExitCode == 0, p.StandardOutput.ReadToEnd(), p.StandardError.ReadToEnd());
+            }
+            catch (System.Exception e)
+            {
+                UnityEngine.Debug.LogException(e);
+                resultCallback(false, "", "");
+            }
         }
     }
 }
